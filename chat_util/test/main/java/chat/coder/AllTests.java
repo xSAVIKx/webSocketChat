db36585915a088938f1211b5cf52ae651eb1d633
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2015-04-21 00:15:43 +0300

    chat_util added test suite

---
+++ chat_util/test/main/java/chat/coder/AllTests.java
@@ -0,0 +1,12 @@
+package chat.coder;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CommandPojoDecoderTest.class, CommandPojoEncoderTest.class,
+		CommandSerializerTest.class, ListCommandPojoEncoderTest.class })
+public class AllTests {
+
+}
